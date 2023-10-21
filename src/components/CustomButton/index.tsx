140f76fef845378ed3eba1b6d06df63068f4e2f0
freddy.liu <freddy.liu@positivegrid.com>
2023-10-22 00:47:11 +0800

    feat: add custom button

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import IF from '../IF';
+import classNames from 'classnames';
+import Link from 'next/link';
+import style from './CustomButton.module.scss';
 
 type CustomButtonProps = {
   children: React.ReactNode;
   buttonType: 'button' | 'anchor' | 'link';
-  className: string;
-  size: 'small' | 'large';
+  type?: 'button' | 'submit' | 'reset';
+  size?: 'small' | 'large';
+  href: string;
   inverse?: boolean;
   danger?: boolean;
   disabled?: boolean;
@@ -15,18 +18,64 @@ type CustomButtonProps = {
 const CustomButton: React.FC<CustomButtonProps> = ({
   children,
   buttonType,
-  className,
+  type,
+  href,
   size,
   inverse,
   danger,
   disabled,
   onClick,
 }) => {
-  return (
-    <IF as={buttonType === 'anchor' ? 'a' : 'button'} className={className}>
-      {children}
-    </IF>
-  );
+  switch (buttonType) {
+    case 'link':
+      return (
+        <Link
+          href={href}
+          className={classNames(
+            style.root,
+            size === 'small' && style.buttonSmall,
+            size === 'large' && style.buttonLarge,
+            inverse && style.buttonInverse,
+            danger && style.buttonDanger,
+          )}
+        >
+          {children}
+        </Link>
+      );
+    case 'anchor':
+      return (
+        <a
+          href={href}
+          className={classNames(
+            style.root,
+            size === 'small' && style.buttonSmall,
+            size === 'large' && style.buttonLarge,
+            inverse && style.buttonInverse,
+            danger && style.buttonDanger,
+          )}
+        >
+          {children}
+        </a>
+      );
+    case 'button':
+    default:
+      return (
+        <button
+          className={classNames(
+            style.root,
+            size === 'small' && style.buttonSmall,
+            size === 'large' && style.buttonLarge,
+            inverse && style.buttonInverse,
+            danger && style.buttonDanger,
+          )}
+          type={type}
+          onClick={onClick}
+          disabled={disabled}
+        >
+          {children}
+        </button>
+      );
+  }
 };
 
 export default CustomButton;
