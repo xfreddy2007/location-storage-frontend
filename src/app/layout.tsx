2fea3697130519e5ef02dff9cc4637a6c4b9cc96
freddy.liu <freddy.liu@positivegrid.com>
2023-10-21 11:33:47 +0800

    feat: add style-jsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 import '../styles/globals.scss';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,30 @@
+'use client';
+
 import style from './Head.module.scss';
 
 const Header = () => {
-  return <div className={style.root}>Header</div>;
+  return (
+    <div>
+      <div className={style.root}>Header</div>
+      <p>
+        asdakjshd
+        <span>HITHITHITHIT</span>
+      </p>
+      <style jsx>{`
+        p {
+          color: red;
+          span {
+            color: blue;
+          }
+        }
+        @media (min-width: 640px) {
+          p {
+            color: green;
+          }
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Header;
